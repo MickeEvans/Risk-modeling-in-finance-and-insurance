496fbdd769a5ff9804c633615703147f39095ef7
MickeEvans <m.sederlundevans@gmail.com>
2026-09-09 09:29:56 +0200

    First commit

---
+++ L1/Code/daily_volatility_forecast.py
@@ -0,0 +1,2 @@
+#This file will answer the first two questions in the report, regarding performing a daily volatility forecast using MA, EWMA and GARCH models. The code will also include a function to calculate the RMSE of the forecasts, and a function to plot the forecasts against the actual volatility.
+
